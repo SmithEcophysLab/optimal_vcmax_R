3eed625c9cacbd1b4ffb00d8f309bde5be33a781
SmithEcophysLab <nick.gregory.smith@gmail.com>
2020-04-22 15:03:12 -0500

    Restructured of the temperature response of the term that describes the quantum efficiency of photosystem II (q0 in the model). The formulation now follows that of Bernacchi et al. (2003)

--- calc_optimal_vcmax.R
+++ calc_optimal_vcmax.R
@@ -13,8 +13,6 @@
 # q0: quantum efficiency of photosynthetic electron transport (mol/mol)
 # theta: curvature of the light response of electron transport (unitless)
 # R: universal gas constant (J mol-1 K-1)
-# tg_K: acclimated temperature (K)
-# to: temperature optimum for vcmax (degC)
 # patm: atmospheric pressure (Pa)
 # ca: atmospheric CO2 at z (Pa)
 # km: Michaelis-Menten constant for Rubisco (Pa)
@@ -53,37 +51,33 @@ calc_optimal_vcmax <- function(tg_c = 25, z = 0, vpdo = 1, cao = 400, paro = 800
 	par <- calc_par(paro, z)
 	vpd <- calc_vpd(tg_c, z, vpdo)
 	ca <- cao * 1e-6 * patm
-	to <- (0.44 * tg_c + 24.92)              # Eq. 21, note: intercept differs due to use of °C
-	tg_K <- tg_c + 273.15
-	
+
 	# K and Gamma* model terms
-    km <- calc_km_pa(tg_c, z)                # Eq. 3
+  km <- calc_km_pa(tg_c, z)                
 	gammastar <- calc_gammastar_pa(tg_c, z)   
 
 	# Coordination and least-cost hypothesis model terms
-	chi <- calc_chi(tg_c, z, vpdo, cao)              # Eq. 1
+	chi <- calc_chi(tg_c, z, vpdo, cao)              
 	ci <- chi * ca # Pa
-	mc <- ((ci - gammastar) / (ci + km))             # Eq. 6
-	m <- ((ci - gammastar)/(ci + (2 * gammastar)))   # Eq. 8
-	omega <- calc_omega(theta = theta, c = c, m = m) # Eq. S4
-	omega_star <- (1 + (omega) - sqrt((1 + (omega))^2 - (4 * theta * omega)))  # Eq. 18
+	mc <- ((ci - gammastar) / (ci + km))             
+	m <- ((ci - gammastar)/(ci + (2 * gammastar)))   
+	omega <- calc_omega(theta = theta, c = c, m = m) 
+	omega_star <- (1 + (omega) - sqrt((1 + (omega))^2 - (4 * theta * omega))) 
+	
+	# calculate q0 using Bernacchi et al. (2003) temperature response (set to 0.257 at 25C)
+	q0 = -0.0805 + (0.022 * tg_c) - (0.00034 * tg_c * tg_c)
 	
 	# calculate vcmax and jmax	
-	vcmax_star <- ((q0 * par * m) / mc) * (omega_star / (8 * theta))	          # Eq. 19
-	vcmax_prime <- vcmax_star * calc_tresp_mult(tg_c, tg_c, tref = to)         # Eq. 20
-	jvrat <- ((8 * theta * mc * omega) / (m * omega_star))     # Eq. 15 / Eq. 19
-	jmax_prime <- jvrat * vcmax_prime
-	
-	# equivalently
-	# jmax_prime <- q0 * par * omega * calc_tresp_mult(tg_c, tg_c, tref = to)    # Eq. 15 * Eq. 20
-	# jvrat <- jmax_prime/vcmax_prime
+	vcmax <- ((q0 * par * m) / mc) * (omega_star / (8 * theta))	          
+	jvrat <- ((8 * theta * mc * omega) / (m * omega_star))     
+	jmax <- jvrat * vcmax
 	
 	# output
-	results <- as.data.frame(cbind(tg_c, z, vpdo, cao, paro, q0, theta, c, to, par, patm, ca, vpd, chi, ci, km, 
-	                               gammastar, omega, m, mc, omega_star, vcmax_star, vcmax_prime, jvrat, jmax_prime))
+	results <- as.data.frame(cbind(tg_c, z, vpdo, cao, paro, q0, theta, c, par, patm, ca, vpd, chi, ci, km, 
+	                               gammastar, omega, m, mc, omega_star, vcmax, jvrat, jmax))
 	
-	colnames(results) <- c('tg_c', 'z', 'vpdo', 'cao', 'paro', 'q0', 'theta', 'c', 'to', 'par', 'patm', 'ca', 'vpd', 'chi', 'ci', 'km', 
-	                       'gammastar', 'omega', 'm', 'mc', 'omega_star', 'vcmax_star', 'vcmax_prime', 'jvrat', 'jmax_prime')
+	colnames(results) <- c('tg_c', 'z', 'vpdo', 'cao', 'paro', 'q0', 'theta', 'c', 'par', 'patm', 'ca', 'vpd', 'chi', 'ci', 'km', 
+	                       'gammastar', 'omega', 'm', 'mc', 'omega_star', 'vcmax', 'jvrat', 'jmax')
 	
 	results	
 	
